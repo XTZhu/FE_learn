25de22f09f7756e99df653e5044fd550c5f3b9e5
XTZhu <a1291753307@qq.com>
2021-12-21 10:56:27 +0800

    update

--- JavaScript高级程序设计/part-6 集合引用类型/index.js
+++ JavaScript高级程序设计/part-6 集合引用类型/index.js
@@ -11,3 +11,67 @@ zeroes.fill(0); // 重置
 console.log(zeroes.fill(6, 6, -3)); // [0, 0, 0, 0, 0]
 zeroes.fill(0); // 重置
 console.log(zeroes);
+
+/* --------------------------------------------------- */
+// 转换方法
+// 所有对象都有 toLocaleString()、toString()和 valueOf()方法
+// valueOf() 返回的还是数组本身
+// 而 toString()返回由数组中每个值的等效字符串拼接而成的一个[逗号]分隔的字符串
+// alert()期待字符串，所以会在后台调用数组的 toString()方法，从而得到跟前面一样的结果。
+
+let colors = ["red", "green", "blue"];
+alert(colors.toString()); // red,green,blue
+alert(colors.valueOf()); // red,green,blue
+alert(colors);
+
+/* --------------------------------------------------- */
+
+let person1 = {
+  toLocaleString() {
+    return "Nikolaos";
+  },
+  toString() {
+    return "Nicholas";
+  },
+};
+
+let person2 = {
+  toLocaleString() {
+    return "joce";
+  },
+  toString() {
+    return "Joey";
+  },
+};
+
+let people = [person1, person2];
+alert(people); //Nicholas,Joey
+alert(people.toString()); //Nicholas,Joey
+alert(people.toLocaleString()); //Nikolaos,joce
+
+/* --------------------------------------------------- */
+let colors = ["red", "green", "blue"];
+alert(colors.join("and")); // redandgreenandblue
+alert(colors.join()); // red,green,blue
+alert(colors.join("|")); // red|green|blue
+let colors = ["red", "green", null, undefined, "blue"];
+alert(colors.join("and")); // redandgreenandandandblue
+alert(colors.join()); // red,green,,,blue
+alert(colors.join("|")); // red|green|||blue
+
+/* --------------------------------------------------- */
+// 栈方法
+// push()、pop()
+// push() 向数组的末尾添加一个或更多元素，并返回新的长度。
+// pop() 从数组的末尾移除一个元素，并返回移除的元素。
+
+// 队列方法
+// shift()、unshift()
+// shift() 从数组的开头移除一个元素，并返回移除的元素。
+// unshift() 向数组的开头添加一个或更多元素，并返回新的长度。
+let colors = new Array();
+let count = colors.push("red", "green", "blue");
+alert(count); // 3
+
+let item = colors.pop();
+alert(item); // blue
