ae98357bcf026f012e601abc3958b3c9ee5da345
XTZhu <a1291753307@qq.com>
2021-12-22 17:56:56 +0800

    update

--- JavaScript高级程序设计/part-6 集合引用类型/index.js
+++ JavaScript高级程序设计/part-6 集合引用类型/index.js
@@ -69,9 +69,70 @@ alert(colors.join("|")); // red|green|||blue
 // shift()、unshift()
 // shift() 从数组的开头移除一个元素，并返回移除的元素。
 // unshift() 向数组的开头添加一个或更多元素，并返回新的长度。
+
+// 删除方法就输出 被删除的项
+// 添加方法就输出 数组的长度
 let colors = new Array();
 let count = colors.push("red", "green", "blue");
-alert(count); // 3
+console.log(count); // 3
 
 let item = colors.pop();
-alert(item); // blue
+console.log(item); // blue
+
+console.log(colors); // red,green
+
+const shiftF = colors.shift();
+console.log(shiftF); // red
+
+const unShiftF = colors.unshift("yellow", "pink");
+console.log(unShiftF); // 3
+
+/* --------------------------------------------------- */
+// 排序方法
+// reverse(); sort();
+let values = [1, 2, 3, 4, 5, 10];
+values.reverse();
+console.log(values); // 5,4,3,2,1
+// sort()会按照升序重新排列数组元素，即最小的值在前面，最大的值在后面。
+let output = values.sort();
+console.log(output);
+
+// 比较函数接收两个参数，如果第一个参数(a)应该排在第二个参数(b)前面，就返回负值；如果两个参数相
+// 等，就返回 0；如果第一个参数(a)应该排在第二个参数(b)后面，就返回正值。
+
+// 简单来说，根据函数返回的值来确定排序的顺序。返回的值越小，a排序越靠前。
+// 如果数组的元素是数值，或者是其 valueOf()方法返回数值的对象（如 Date 对象），这个比较函
+// 数还可以写得更简单，因为这时可以直接用第二个值减去第一个值：
+output = values.sort((a, b) => a - b);
+console.log(output);
+
+// reverse()和 sort()都返回调用它们的数组的引用。
+
+/* --------------------------------------------------- */
+// 操作方法
+// concat()、slice()、splice()
+// concat() 合并数组，并返回新数组。
+let colors = ["red", "green", "blue"];
+let colors2 = colors.concat("yellow", ["black", "brown"]);
+console.log(colors); // ["red", "green","blue"]
+console.log(colors2); // ["red", "green", "blue", "yellow", "black", "brown"]
+
+// slice() 方法返回一个数组的一段，两个参数，第一个参数是开始位置，第二个参数是结束位置。
+// 如果第一个参数是负数，那么它表示倒数的位置，
+// 如果第一个参数是 0，那么它表示数组的开头，
+// 如果第一个参数是正数，那么它表示数组的第几个位置，
+// 如果第二个参数是负数，那么它表示倒数的位置，
+// 如果第二个参数是 0，那么它表示数组的结尾。
+// 根据index来截取， 含头不含尾
+
+let colors = ["red", "green", "blue", "yellow", "black", "brown"];
+let output = colors.slice(1, 4);
+console.log(output); // green,blue,yellow
+
+// splice() 方法用于替换数组的一部分，并返回被删除的元素。
+// 它有三个参数，第一个参数是开始位置，第二个参数是删除的个数，第三个参数是插入的值。
+// 如果没有第三个参数，那么就是删除。
+// 如果第三个参数是一个数组 || undefined || null || 函数 || 字符串 || 数字，那么就是插入。
+let colors = ["red", "green", "blue", "yellow", "black", "brown"];
+let output = colors.splice(1, 3, "orange", "purple");
+console.log(output); // green,blue,yellow
